0787749a138cbd449dac84da04b6cf07eba1da6a
Marco Franssen <marco.franssen@gmail.com>
2018-08-17 02:02:33 +0200

    Wait exiting the process for SIGHUP SIGQUIT SIGKILL

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/marcofranssen/gothermostat/config"
 	"github.com/marcofranssen/gothermostat/nest"
@@ -18,23 +21,39 @@ const (
 )
 
 func main() {
-	cfg := config.New()
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
 
-	err := cfg.Load(configFile)
-	check(err)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGKILL)
 
-	n := nest.New(cfg)
-	err = n.Authenticate()
-	check(err)
-	cfg.Save(configFile)
+	go func() {
+		cfg := config.New()
 
-	var response nest.Combined
-	err = n.All(&response)
-	check(err)
+		err := cfg.Load(configFile)
+		check(err)
 
-	fmt.Printf("UserID: %s\nAccessToken: %s\nClientVersion: %v\n", response.Metadata.UserID, response.Metadata.AccessToken, response.Metadata.ClientVersion)
-	for _, thermostat := range response.Devices.Thermostats {
-		fmt.Printf("Thermostat: '%s', temperature %v", thermostat.Name, thermostat.AmbientTemperatureC)
-	}
+		n := nest.New(cfg)
+		err = n.Authenticate()
+		check(err)
+		cfg.Save(configFile)
 
+		var response nest.Combined
+		err = n.All(&response)
+		check(err)
+
+		fmt.Printf("UserID: %s\nAccessToken: %s\nClientVersion: %v\n", response.Metadata.UserID, response.Metadata.AccessToken, response.Metadata.ClientVersion)
+		for _, thermostat := range response.Devices.Thermostats {
+			fmt.Printf("Thermostat: '%s', temperature %v", thermostat.Name, thermostat.AmbientTemperatureC)
+		}
+	}()
+
+	fmt.Println("Waiting for you to close")
+	fmt.Println()
+
+	sig := <-sigs
+	fmt.Println()
+	fmt.Println(sig)
+
+	<-done
+	fmt.Println("Exiting")
 }
