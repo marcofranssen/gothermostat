0c019643bd915b3169c4c8d4af67b26cd74658cd
Marco Franssen <marco.franssen@gmail.com>
2018-08-18 14:00:59 +0200

    Add tests for config package

---
+++ config/config_test.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"io/ioutil"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const configFilePath = "../dist.config.json"
+
+func TestLoadConfig(t *testing.T) {
+	Convey("Given loading a config file", t, func() {
+		config := New()
+		So(config, ShouldNotBeNil)
+		config.Load(configFilePath)
+
+		Convey("Config values should be there", func() {
+			So(config.AuthURL, ShouldNotBeEmpty)
+			So(config.TokenURL, ShouldNotBeEmpty)
+			So(config.ClientID, ShouldBeEmpty)
+			So(config.ClientSecret, ShouldBeEmpty)
+			So(config.AuthCode, ShouldBeEmpty)
+			So(config.AccessToken, ShouldBeEmpty)
+		})
+	})
+}
+func TestSaveConfig(t *testing.T) {
+	Convey("Given saving a config file", t, func() {
+		config := New()
+		config.Load(configFilePath)
+		So(config, ShouldNotBeNil)
+
+		Convey("Given the config is saved", func() {
+			loadedConfigJSON, _ := jsonMarshal(config)
+			config.Save(configFilePath)
+			savedConfigJSON, _ := ioutil.ReadFile(configFilePath)
+			So(string(loadedConfigJSON), ShouldEqual, string(savedConfigJSON))
+		})
+	})
+}
