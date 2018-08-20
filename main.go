e90ee7eeccaad6533deed4f229b2792694e65c0d
Marco Franssen <marco.franssen@gmail.com>
2018-08-20 21:22:05 +0200

    Make scheduled request every 15 minutes

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/marcofranssen/gothermostat/config"
 	"github.com/marcofranssen/gothermostat/nest"
@@ -21,6 +23,8 @@ const (
 )
 
 func main() {
+	myContext, cancel := context.WithCancel(context.Background())
+
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
 
@@ -37,12 +41,11 @@ func main() {
 		check(err)
 		cfg.Save(configFile)
 
-		var response nest.Combined
-		err = n.All(&response)
-		check(err)
-
+		response := getData(n)
 		fmt.Printf("UserID: %s\nAccessToken: %s\nClientVersion: %v\n", response.Metadata.UserID, response.Metadata.AccessToken, response.Metadata.ClientVersion)
-		printThermostatData(response.Devices.Thermostats)
+		printThermostatData(time.Now(), response.Devices.Thermostats)
+
+		schedule(myContext, n, 15*time.Minute)
 	}()
 
 	fmt.Println("Waiting for you to close")
@@ -51,13 +54,36 @@ func main() {
 	sig := <-sigs
 	fmt.Println()
 	fmt.Println(sig)
+	cancel()
 
 	<-done
 	fmt.Println("Exiting")
 }
 
-func printThermostatData(thermostats map[string]*nest.Thermostat) {
+func schedule(ctx context.Context, nest nest.Nest, refreshTime time.Duration) {
+	ticker := time.NewTicker(refreshTime)
+	go func() {
+		for tick := range ticker.C {
+			response := getData(nest)
+			printThermostatData(tick, response.Devices.Thermostats)
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		fmt.Println("Stopping ticker")
+		ticker.Stop()
+	}
+}
+
+func getData(myNest nest.Nest) nest.Combined {
+	var response nest.Combined
+	err := myNest.All(&response)
+	check(err)
+	return response
+}
+
+func printThermostatData(tick time.Time, thermostats map[string]*nest.Thermostat) {
 	for _, thermostat := range thermostats {
-		fmt.Printf("Thermostat: '%s', temperature %v", thermostat.Name, thermostat.AmbientTemperatureC)
+		fmt.Printf("%s | Thermostat: '%s', temperature %v\n", tick.Format(time.RFC3339), thermostat.Name, thermostat.AmbientTemperatureC)
 	}
 }
