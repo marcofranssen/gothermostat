3b013b1e4ba24821c2e4383f45dbe03d8f4fdfe3
Marco Franssen <marco.franssen@gmail.com>
2018-08-28 17:19:04 +0200

    Persist thermostat data

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
@@ -20,6 +22,7 @@ func check(err error) {
 
 const (
 	configFile = "./config.json"
+	dataFile   = "./thermodata.json"
 )
 
 func main() {
@@ -42,10 +45,12 @@ func main() {
 		cfg.Save(configFile)
 
 		response := getData(n)
+		now := time.Now()
 		fmt.Printf("UserID: %s\nAccessToken: %s\nClientVersion: %v\n", response.Metadata.UserID, response.Metadata.AccessToken, response.Metadata.ClientVersion)
-		printThermostatData(time.Now(), response.Devices.Thermostats)
+		printThermostatData(now, response.Devices.Thermostats)
+		saveTemperatureResult(now, response.Devices.Thermostats)
 
-		schedule(myContext, n, 15*time.Minute)
+		schedule(myContext, n, 5*time.Minute)
 	}()
 
 	fmt.Println("Waiting for you to close")
@@ -66,6 +71,7 @@ func schedule(ctx context.Context, nest nest.Nest, refreshTime time.Duration) {
 		for tick := range ticker.C {
 			response := getData(nest)
 			printThermostatData(tick, response.Devices.Thermostats)
+			saveTemperatureResult(tick, response.Devices.Thermostats)
 		}
 	}()
 	select {
@@ -80,6 +86,50 @@ func getData(myNest nest.Nest) nest.Combined {
 	err := myNest.All(&response)
 	check(err)
 	return response
+}
+
+func saveTemperatureResult(tick time.Time, thermostats map[string]*nest.Thermostat) {
+	data, _ := readFile(dataFile)
+	var temperatureData []thermoData
+	json.Unmarshal(data, &temperatureData)
+	for _, thermostat := range thermostats {
+		temperatureData = append(temperatureData, thermoData{
+			tick,
+			thermostat.Name,
+			thermostat.AmbientTemperatureC,
+			thermostat.AmbientTemperatureF,
+		})
+		bytes, err := json.Marshal(&temperatureData)
+		check(err)
+		err = writeFile(dataFile, bytes)
+		check(err)
+	}
+}
+
+func writeFile(filePath string, data []byte) error {
+	file, err := os.Create(filePath)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(data)
+
+	return err
+}
+
+func readFile(filePath string) ([]byte, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+type thermoData struct {
+	Timestamp    time.Time `json:"timestamp"`
+	Thermostat   string    `json:"thermostat"`
+	TemperatureC float64   `json:"temperatureC"`
+	TemperatureF int       `json:"temperatureF"`
 }
 
 func printThermostatData(tick time.Time, thermostats map[string]*nest.Thermostat) {
