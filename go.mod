af05d47a0419abac22e03664b4f46469c9d322d6
Marco Franssen <marco.franssen@gmail.com>
2019-07-30 09:05:17 +0200

    Refactored gracefull shutdown and utilize zap.Logger

--- go.mod
+++ go.mod
@@ -4,6 +4,11 @@ go 1.12
 
 require (
 	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/smartystreets/assertions v1.0.1 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190710185942-9d28bd7c0945
+	github.com/stretchr/testify v1.3.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
 )
--- server.go
+++ server.go
@@ -1,12 +1,74 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/marcofranssen/gothermostat/config"
+
+	"go.uber.org/zap"
 )
+
+// Server provides an http.Server
+type Server struct {
+	logger *zap.Logger
+	*http.Server
+}
+
+// NewServer creates a new instance of a server and configures the routes
+func NewServer(cfg config.WebserverConfig, logger *zap.Logger) (*Server, error) {
+	r := &http.ServeMux{}
+	r.Handle("/", http.FileServer(http.Dir("./web/build")))
+	r.HandleFunc("/ping", ping)
+	r.HandleFunc("/api/thermostat-data", api)
+
+	errorLog, _ := zap.NewStdLogAt(logger, zap.ErrorLevel)
+	srv := http.Server{
+		Addr:         cfg.Address,
+		Handler:      r,
+		ErrorLog:     errorLog,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{logger, &srv}, nil
+}
+
+// Start runs ListenAndServe on the http.Server with graceful shutdown
+func (srv *Server) Start() {
+	srv.logger.Info("Starting server...")
+	defer srv.logger.Sync()
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			srv.logger.Fatal("Could not listen on", zap.String("addr", srv.Addr), zap.Error(err))
+		}
+	}()
+	srv.logger.Info("Server is ready to handle requests", zap.String("addr", srv.Addr))
+	srv.gracefullShutdown()
+}
+
+func (srv *Server) gracefullShutdown() {
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, os.Interrupt)
+	sig := <-quit
+	srv.logger.Info("Server is shutting down", zap.String("reason", sig.String()))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	srv.SetKeepAlivesEnabled(false)
+	if err := srv.Shutdown(ctx); err != nil {
+		srv.logger.Fatal("Could not gracefully shutdown the server", zap.Error(err))
+	}
+	srv.logger.Info("Server stopped")
+}
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong"))
@@ -33,13 +95,4 @@ func api(w http.ResponseWriter, r *http.Request) {
 	} else {
 		jsonWrite(w, data)
 	}
-}
-
-func webserver(config config.WebserverConfig) error {
-	http.Handle("/", http.FileServer(http.Dir("./web/build")))
-	http.HandleFunc("/ping", ping)
-	http.HandleFunc("/api/thermostat-data", api)
-
-	fmt.Printf("Starting webserver at %s\n", config.Address)
-	return http.ListenAndServe(config.Address, nil)
 }
