7a0668ed00777b1fc3d4b643cd78584c31d8d383
Marco Franssen <marco.franssen@gmail.com>
2019-08-13 23:03:19 +0200

    Apply fixes for deb/rpm {pre,post}install.sh and preremove.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Application permissions"
+echo "- Application permissions"
+chown -R root:adm /var/log/marcofranssen
 chown -R gothermostat:gothermostat {/etc,/var/log}/marcofranssen/gothermostat
-chmod +s {/etc,/var/log}/marcofranssen/gothermostat
+# chmod +s {/etc,/var/log}/marcofranssen/gothermostat
+chmod +x /usr/local/bin/gotherm
 
 # restore config backup if exists
-cp /etc/marcofranssen/gothermostat/.gotherm.toml.bak /etc/marcofranssen/gothermostat/.gotherm.toml 2> /dev/null
+cp /etc/marcofranssen/gothermostat/.gotherm.toml.bak /etc/marcofranssen/gothermostat/.gotherm.toml || true 2>/dev/null
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -2,11 +2,11 @@
 
 # Create application user
 echo -n "- Creating application user "
-if [ `id -u "gothermostat" 2> /dev/null || echo -1` -ge 0 ]; then
+if [ $(id -u "gothermostat" 2>/dev/null || echo -1) -ge 0 ]; then
     echo "SKIP: User already exists"
 else
     sudo useradd -r -s /bin/false gothermostat
 fi
 
 # make backup of config if exists
-cp /etc/marcofranssen/gothermostat/.gotherm.toml /etc/marcofranssen/gothermostat/.gotherm.toml 2> /dev/null
+cp /etc/marcofranssen/gothermostat/.gotherm.toml /etc/marcofranssen/gothermostat/.gotherm.toml.bak || true 2>/dev/null
